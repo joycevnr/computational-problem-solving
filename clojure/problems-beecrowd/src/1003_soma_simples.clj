b18ec1b8f6840d9ff74a1aa40da5af6d7c90ec81
Joyce Vitória <joycevitoria150@gmail.com>
2025-05-01 15:26:15 -0300

    add problem

---
+++ clojure/problems-beecrowd/src/1003_soma_simples.clj
@@ -0,0 +1,5 @@
+(def A (Integer/parseInt (read-line)))
+(def B (Integer/parseInt (read-line)))
+(def soma (+ A B))
+
+(println "SOMA =" soma)
---
+++ clojure/problems-beecrowd/src/1004_pruduto_simples.clj
@@ -0,0 +1,5 @@
+(def A (Integer/parseInt (read-line)))
+(def B (Integer/parseInt (read-line)))
+(def mult (* A B))
+
+(println "PROD =" mult)
