6a09b5d026a55029d76f6190d077bc53e55eeebc
Joyce Vitória Nascimento Rodrigues <128332641+joycevnr@users.noreply.github.com>
2025-06-25 22:21:44 -0300

    Update MergeSortPasso.java

--- java/leda/MergeSortPasso.java
+++ java/leda/MergeSortPasso.java
@@ -18,7 +18,7 @@ public class MergeSortPasso {
     public void mergeSort(int[] v, int ini, int fim){
         if(ini < fim){
             int meio = (ini + fim) / 2;
-            System.out.println(Arrays.toString(v);
+            System.out.println(Arrays.toString(v));
             mergeSort(v, ini, meio);
             mergeSort(v, meio+1, fim);
             merge(v, ini, fim);
@@ -33,7 +33,7 @@ public class MergeSortPasso {
         for(int i = 0; i < v.length; i++){
             helper[i] = v[i];
         }
-        System.out.println(Arrays.toString(v);
+        System.out.println(Arrays.toString(v));
         int i = ini; //primeira metade
         int meio = (ini + fim) / 2; //pivot
         int k = ini; //posições do array
@@ -47,6 +47,6 @@ public class MergeSortPasso {
         while(ini <= meio){//add o restante da primeira parte se sobrar
             v[k++] = helper[i++];
         }
-        System.out.println(Arrays.toString(v);
+        System.out.println(Arrays.toString(v));
     }
 }
